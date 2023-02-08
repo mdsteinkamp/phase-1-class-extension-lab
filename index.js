da7378f92581db80f39af1e4aab339d61c25a147
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-02-07 18:22:40 -0800

    commit tree 00e11935ac802801d23e93e72be808ebb8e1562c

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+class Polygon {
+
+    constructor(side) {
+        this.side = side
+    }
+
+    get countSides() {
+        return this.side.length
+    }
+
+    get perimeter() {
+        return this.side.reduce((a, c) => a + c, 0)
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        const sides = this.side.map(num =>num).sort((a, b) => a - b)
+        return sides[0] + sides[1] > sides[2] ? true : false
+    }
+
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        return new Set(this.side).size === 1
+    }
+
+    get area() {
+        return this.side[0] * this.side[0]
+    }
+
+}
